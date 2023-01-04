5a12ccf865bfee87912cd259e010fb94fdd9c248
Aaron Salisbury <aaron.salisbury@live.com>
2023-01-03 19:42:03 -0600

    Added some notes about potential future implementations of speech recognizers.

--- AtaraxiaAI.Business/Services/Speech/SpeechToText/IRecognizer.cs
+++ AtaraxiaAI.Business/Services/Speech/SpeechToText/IRecognizer.cs
@@ -9,5 +9,11 @@ namespace AtaraxiaAI.Business.Services
         void Shutdown();
 
         void Listen(Action<string> speechRecognizedAction);
+
+        //TODO: Potential future implementations:
+        //    https://fosspost.org/open-source-speech-recognition/
+        //    Syn.Speech  |  Cross-platform but currently relies on .Net Framework. https://developer.syn.co.in/tutorial/speech/recognition-without-grammar.html
+        //    Vosk        |  So far it looks like it requires a wav file to be sent and can't just always listen. https://alphacephei.com/vosk/
+        //    DeepSpeech  |  Very complicated. The DeepSpeach libraries would probably work, but the pbmm model exceeds github limit and DeepSpeech-TFLite relies on .Net Framework https://deepspeech.readthedocs.io/en/r0.9/DotNet-Examples.html
     }
 }
