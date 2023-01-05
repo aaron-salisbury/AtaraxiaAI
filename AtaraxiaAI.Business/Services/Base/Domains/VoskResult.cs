185eb8ee3b427f343f7873b64c80ce8583f8276d
Aaron Salisbury <aaron.salisbury@live.com>
2023-01-05 08:58:42 -0600

    More Vosk work.

---
+++ AtaraxiaAI.Business/Services/Base/Domains/VoskResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AtaraxiaAI.Business.Services.Base.Domains
+{
+    [Serializable]
+    internal class VoskResult
+    {
+        public double Conf { get; set; }
+        public double End { get; set; }
+        public double Start { get; set; }
+        public string Word { get; set; }
+    }
+}
--- AtaraxiaAI.Business/Services/Speech/SpeechToText/VoskRecognizer.cs
+++ AtaraxiaAI.Business/Services/Speech/SpeechToText/VoskRecognizer.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AtaraxiaAI.Business.Services.Base.Domains;
+using AtaraxiaAI.Data.Base;
+using System;
 using System.IO;
+using System.Text;
 using Vosk;
 
 namespace AtaraxiaAI.Business.Services
@@ -8,33 +11,43 @@ namespace AtaraxiaAI.Business.Services
     // https://github.com/alphacep/vosk-api/blob/master/csharp/demo/VoskDemo.cs
     internal class VoskRecognizer : IRecognizer
     {
+        private Vosk.VoskRecognizer _recognizer;
+
         internal VoskRecognizer()
         {
             Vosk.Vosk.SetLogLevel(-1);
-            Model model = new Model(Data.CRUD.ReadVoskModelContentPath());
+
+            Vosk.VoskRecognizer _recognizer = new Vosk.VoskRecognizer(
+                new Model(Data.CRUD.ReadVoskModelContentPath()), 
+                16000.0f);
+
+            _recognizer.SetMaxAlternatives(0);
+            _recognizer.SetWords(true);
+
+
 
             // Demo byte buffer
-            Vosk.VoskRecognizer rec = new Vosk.VoskRecognizer(model, 16000.0f);
-            rec.SetMaxAlternatives(0);
-            rec.SetWords(true);
+            StringBuilder recognizedWordsBuilder = new StringBuilder();
+
             using (Stream source = File.OpenRead("./Detection/Voice/Vosk/test.wav"))
             {
                 byte[] buffer = new byte[4096];
                 int bytesRead;
                 while ((bytesRead = source.Read(buffer, 0, buffer.Length)) > 0)
                 {
-                    if (rec.AcceptWaveform(buffer, bytesRead))
+                    if (_recognizer.AcceptWaveform(buffer, bytesRead))
                     {
-                        AI.Log.Logger.Information($"RESULT: {rec.Result()}");
-                    }
-                    else
-                    {
-                        AI.Log.Logger.Information($"PARTIAL RESULT: {rec.PartialResult()}");
+                        VoskResultRoot result = Json.ToObjectAsync<VoskResultRoot>(_recognizer.Result()).Result;
+                        recognizedWordsBuilder.Append(result.Text);
+                        recognizedWordsBuilder.Append(' ');
                     }
                 }
             }
 
-            AI.Log.Logger.Information($"FINAL RESULT: {rec.FinalResult()}");
+            VoskResultRoot finalResult = Json.ToObjectAsync<VoskResultRoot>(_recognizer.FinalResult()).Result;
+            recognizedWordsBuilder.Append(finalResult.Text);
+
+            AI.Log.Logger.Information($"*Heard* \"{recognizedWordsBuilder}\"");
         }
 
         bool IRecognizer.IsAvailable()
