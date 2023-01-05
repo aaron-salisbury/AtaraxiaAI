8e100e729b03e70a1d9d1a54a6a5b3d2d1f76921
Aaron Salisbury <aaron.salisbury@live.com>
2023-01-04 22:23:49 -0600

    Download large model files the first time the app runs.

---
+++ AtaraxiaAI.Business/Base/Extensions/StringExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AtaraxiaAI.Business.Base.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Compares sentences and finds the sentence that has the most words in common.
+        /// </summary>
+        public static string FindBestMatch(this string stringToCompare, HashSet<string> stringsToCompareAgainst)
+        {
+            // https://stackoverflow.com/questions/13793560/find-closest-match-to-input-string-in-a-list-of-strings
+
+            HashSet<string> strCompareHash = stringToCompare.Split(' ').ToHashSet();
+
+            int maxIntersectCount = 0;
+            string bestMatch = string.Empty;
+
+            foreach (string str in stringsToCompareAgainst)
+            {
+                HashSet<string> strHash = str.Split(' ').ToHashSet();
+
+                int intersectCount = strCompareHash.Intersect(strCompareHash).Count();
+
+                if (intersectCount > maxIntersectCount)
+                {
+                    maxIntersectCount = intersectCount;
+                    bestMatch = str;
+                }
+            }
+
+            return bestMatch;
+        }
+    }
+}
--- AtaraxiaAI.Business/Services/Vision/PWCYoloVisionEngine.cs
+++ AtaraxiaAI.Business/Services/Vision/PWCYoloVisionEngine.cs
@@ -29,8 +29,6 @@ namespace AtaraxiaAI.Business.Services
 
             try
             {
-                // Got files from https://pjreddie.com/darknet/yolo/
-                // Using the "tiny" version since the regular weights file exceeds github file size limit.
                 _net = DnnInvoke.ReadNetFromDarknet(CRUD.ReadYoloCFGBuffer(), CRUD.ReadYoloWeightsBuffer());
                 _net.SetPreferableBackend(Emgu.CV.Dnn.Backend.OpenCV);
                 _net.SetPreferableTarget(Emgu.CV.Dnn.Target.Cpu);
